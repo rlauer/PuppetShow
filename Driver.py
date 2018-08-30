fc46cade69ff21c0b004f3976f3147590c466131
Ryan <lauerrj@gmail.com>
2018-08-30 17:58:36 -0400

    Finished working on provided test case

--- Driver.py
+++ Driver.py
@@ -1,22 +1,20 @@
-from SeatingChart import SeatingChart
+from SeatingChart import SeatingChart, Seat
 
 CHART_ROWS = 3
 CHART_SEATS = 11
 
+# Get the row and seat out of the input string
 def parseReservation(reservation):
     (row, seat) = reservation.strip('R').split('C')
     return (int(row), int(seat))
 
+
+# Initialize Chart
 chart = SeatingChart(CHART_ROWS, CHART_SEATS)
 
+
+# Initial Reservations
 reservationInput = input()
-seatList = []
-while True:
-    numSeats = input()
-    if numSeats:
-        seatList.append(int(numSeats))
-    else:
-        break
 
 reservations = []
 for reservation in reservationInput.split(' '):
@@ -24,7 +22,28 @@ for reservation in reservationInput.split(' '):
     chart.ReserveSeat(row, seat)
 
 
-print(seatList)
+# Find Best Seats
+while True:
+    numSeats = input()
+    if numSeats:
+        seats = chart.FindBestSeats(int(numSeats))
+        if seats == -1:
+            print("Not Available")
+        elif len(seats) == 1:
+            seat = seats[0]
+            chart.ReserveSeat(seat[0], seat[1])
+            print(f"R{seat[0]}C{seat[1]}")
+        else:
+            for seat in seats:
+                chart.ReserveSeat(seat[0], seat[1])
+            seats.sort(key = lambda s: s[1]) # order by seat
+            rowNumber = seats[0][0]
+            print(f"R{rowNumber}C{seats[0][1]} - R{rowNumber}C{seats[len(seats) - 1][1]}")
+    else:
+        print(chart.TotalUnreservedSeats())
+        break
 
-for i in range(CHART_ROWS):
-    print(f"{i + 1}: {chart.chart[i]}")
+
+# Show reserved seats (for debugging)
+# for i in range(CHART_ROWS):
+#     print(f"{i + 1}: {['X' if seat.reserved else 'O' for seat in chart.chart[i]]}")
--- SeatingChart.py
+++ SeatingChart.py
@@ -1,14 +1,98 @@
+class Seat:
+    def __init__(self, distance):
+        self.reserved = False
+        self.distance = distance
+
 class SeatingChart:
+    distanceList = []
+
 
     def __init__(self, rows, seats):
-        self.chart = [[0 for i in range(seats)] for j in range(rows)]
+        self.rows = rows
+        self.seats = seats
+        self.bestLocation = (1, (seats + 1) / 2.0)
+
+        # Initialize seat array including availability and how ideal the location is
+        self.chart = [
+            [
+                Seat(self.calculateManhattanDistance(i + 1, j + 1))
+                for j in range(seats)
+            ]
+            for i in range(rows)
+        ]
+
+        # Create list of seats ordered by how ideal they are
+        for row in range(rows):
+            for seat in range(seats):
+                self.distanceList.append((row + 1, seat + 1, self.calculateManhattanDistance(row + 1, seat + 1)))
+        
+        self.distanceList.sort(key=lambda seat: seat[2])
+
 
     def ReserveSeat(self, row, seat):
         if row > 0 and seat > 0 and row <= len(self.chart) and seat <= len(self.chart[row - 1]):
-            self.chart[row - 1][seat - 1] = 1
+            self.chart[row - 1][seat - 1].reserved = True
+
     
     def CheckSeatReserved(self, row, seat):
         if row > 0 and seat > 0 and row <= len(self.chart) and seat <= len(self.chart[row - 1]):
-            return self.chart[row - 1][seat - 1]
+            return self.chart[row - 1][seat - 1].reserved
         else:
-            return 0
+            return False
+
+
+    def TotalUnreservedSeats(self):
+        totalSeats = 0
+        
+        for row in self.chart:
+            for seat in row:
+                if not seat.reserved: totalSeats += 1
+        
+        return totalSeats
+
+    
+    def calculateManhattanDistance(self, row, seat):
+        (bestRow, bestSeat) = self.bestLocation
+        return abs(bestRow - row) + abs(bestSeat - seat)
+
+
+    def getManhattanDistance(self, row, seat):
+        return self.chart[row - 1][seat - 1].distance
+
+
+    def getSurroundingSeats(self, row, seat, numSeats):
+        seatList = []
+
+        # Check current seat and ones to the right
+        for i in range(seat, self.seats + 1):
+            if self.CheckSeatReserved(row, i):                
+                break
+            else:
+                seatList.append((row, i, self.getManhattanDistance(row, i)))
+
+        # Original seat was taken, exit
+        if len(seatList) == 0: return []
+
+        # Check seats to the left of current seat
+        for i in range(seat - 1, 1, -1):
+            if self.CheckSeatReserved(row, i):
+                break
+            else:
+                seatList.append((row, i, self.getManhattanDistance(row, i)))
+
+        return seatList
+
+    
+    def FindBestSeats(self, numSeats):
+        # Iterate from best to worst seats
+        for (row, seat, _) in self.distanceList:
+            seats = self.getSurroundingSeats(row, seat, numSeats)
+            if len(seats) == numSeats:
+                return seats
+            elif len(seats) > numSeats:
+                seats.sort(key = lambda s: s[2])
+                return seats[:numSeats]
+        
+        return -1
+
+    
